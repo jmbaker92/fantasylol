63566bfb78e341f747cc992358536b8823d181f7
jmbaker92 <jmbaker92@outlook.com>
2023-09-28 14:05:12 -0600

    add user signup

--- league/views.py
+++ league/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+def signup(request):
+    message = ""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            print("form is valid")
+            form.save()
+            message = "User Created!"
+        else:
+            message = form.errors
+    else:
+        form = UserCreationForm()
+
+    context = {"form": form, "message": message}
+    return render(request, "league/signup.html", context)
