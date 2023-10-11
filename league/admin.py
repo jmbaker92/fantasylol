d42d0045eeebb688abf837de25fa3b4578e07b15
jmbaker92 <jmbaker92@outlook.com>
2023-10-11 14:21:10 -0600

    added player and lol team models

--- league/admin.py
+++ league/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Team
+from .models import Team, LolTeam, Player
 
 admin.site.register(Team)
+admin.site.register(LolTeam)
+admin.site.register(Player)
 # Register your models here.
--- league/models.py
+++ league/models.py
@@ -6,3 +6,37 @@ from django.contrib.auth.models import User
 class Team(models.Model):
     name = models.CharField(max_length=30)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class LolTeam(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Player(models.Model):
+    TOP = "TP"
+    JUNGLE = "JG"
+    MID = "MD"
+    ADC = "AD"
+    SUPPORT = "SP"
+    POSITION_CHOICES = [
+        (TOP, "Top"),
+        (JUNGLE, "Jungle"),
+        (MID, "Mid"),
+        (ADC, "ADC"),
+        (SUPPORT, "Support"),
+    ]
+    name = models.CharField(max_length=30)
+    lol_team = models.ForeignKey(LolTeam, on_delete=models.DO_NOTHING)
+    postion = models.CharField(
+        max_length=2,
+        choices=POSITION_CHOICES,
+    )
+
+    def __str__(self) -> str:
+        return self.name
